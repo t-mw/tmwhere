74efc346fef0fb6069ba985f2638f623e1eb20c3
Tobias Mansfield-Williams <contact@tmwhere.com>
2018-09-30 12:43:16 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # using tidy 5.6.0 from macports
 hugo
-tidy -i -m -w 160 -ashtml -utf8 public/index.html
-tidy -i -m -w 160 -ashtml -utf8 public/city_generation.html
+tidy -im -w 160 --tidy-mark no public/index.html
+tidy -im -w 160 --tidy-mark no public/city_generation.html
 rm public/**/{thumb,1,2,3}.*
 rm -r public/downloads
