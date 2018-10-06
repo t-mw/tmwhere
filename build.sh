345a380326108f65bb33e018ad90b02a6015dbb3
Tobias Mansfield-Williams <contact@tmwhere.com>
2018-10-06 15:39:15 +0200

    Add life drawings

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 # using tidy 5.6.0 from macports
 hugo
-tidy -im -w 160 --tidy-mark no public/index.html
-tidy -im -w 160 --tidy-mark no public/city_generation.html
+tidy -im -w 160 --tidy-mark no public/*.html
 rm public/**/{thumb,1,2,3}.*
 rm -r public/downloads
---
+++ content/life-drawing.md
@@ -0,0 +1,8 @@
++++
+title = "Life Drawing"
+date = 2018-10-06T15:01:41+02:00
+css = "life-drawing"
+slug = "life-drawing"
+layout = "gallery"
++++
+
